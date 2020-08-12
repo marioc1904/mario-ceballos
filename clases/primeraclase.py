f1ba024063478b0953ce0f1feb3043013922b6a4
marioc1904 <mceballos100@gmail.com>
2020-08-12 13:22:52 -0500

    subiendo clase 2

--- clases/primeraclase.py
+++ clases/primeraclase.py
@@ -4,3 +4,7 @@ estatura = "1.76"
 print (nombre)
 print (edad)
 print (estatura)
+
+print (type(nombre))
+print (type(edad))
+print (type(estatura))
---
+++ clases/segunda_clase.py
@@ -0,0 +1,20 @@
+nombre = input("por favor ingrese nombre: ") 
+
+edad = "19"
+estatura = "1.76"
+print ("hola", nombre, "eres bienvenido a este codigo")
+edad= int (input("por favor ingrese su edad: "))
+estatura= float(input("por favor ingrese su estatura: "))
+print (edad)
+print (estatura)
+print (type(nombre))
+print (type(edad))
+print (type(estatura))
+if (edad >= 18) :
+    print("usted es mayor de edad")
+    print ("Que Dios me lo bendiga, que se fueque ")
+
+if (estatura >=1.70) :
+    print ("Eavemaria que man tan alto")
+
+print ("Genial! que te vaya excelente")
